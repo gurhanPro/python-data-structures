ffb5112af63d3e76f645590696af67c7df8b9ec0
Ziyaad Ahmed Gurhan <ziyaadyare@gmail.com>
2018-12-10 16:06:03 +0200

    Update anagram_check.py
    added a way to check anagram by using dictionary instead of sorted()

--- anagram_check.py
+++ anagram_check.py
@@ -10,3 +10,34 @@ question: Given two strings, check if they are anagrams
     s2 = sorted(ss.replace(" ","").lower())
             
     return s1!=s2 # returns true if the strings are the same else false
+
+
+########################################
+#longer version
+# using dictionary 
+# for every unique character in the string we will add it to dictionary and the key will be the number of times it appears
+
+def anagram(s,ss):
+    
+    s = s.replace(" ","").lower()
+    ss = ss.replace(" ","").lower()
+    if len(s) != len(ss):
+        return False
+    dic = {}
+    for l in s:
+        if l in dic:
+            dic[l]+=1
+        else:
+            dic[l] = 1
+    print(dic)        
+    for l in ss:
+        if l in dic:
+            dic[l]-= 1
+        else:
+            dic[l] = 1
+            
+    for i in dic:
+        if dic[i]!=0:
+            return False
+        else:
+            return True
