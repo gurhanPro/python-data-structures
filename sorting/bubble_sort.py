444a67c8b770bbfc54fc754259a92ebcf8ac4912
Ziyaad Ahmed Gurhan <ziyaadyare@gmail.com>
2019-01-02 06:41:44 +0200

    Create bubble_sort.py

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(arr):
+    for n in range(len(arr)-1,0,-1):
+        for k in range(n):
+            if arr[k] > arr[k+1]:
+                temp = arr[k+1]
+                arr[k+1] = arr[k]
+                arr[k] = temp
